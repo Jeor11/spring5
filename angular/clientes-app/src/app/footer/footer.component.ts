9263a76060ee44a6e1a2af34d30d41bd76bdceb9
Joel <jeor_eli@hotmail.com>
2021-09-14 21:27:24 -0500

    Creamos un atributo para el footer.

--- angular/clientes-app/src/app/footer/footer.component.ts
+++ angular/clientes-app/src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+public autor: any = {nombre: 'Joel', apellido: 'Olguín'};
   constructor() { }
 
   ngOnInit(): void {
