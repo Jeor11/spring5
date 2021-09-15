a0e876c047ac8b5510c42fdb661e9aed65eda9b4
Joel <jeor_eli@hotmail.com>
2021-09-14 20:44:04 -0500

    Se agrego la ruta de los directorios

--- angular/clientes-app/src/app/app.module.ts
+++ angular/clientes-app/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ClientesComponent,
+    DirectivaComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
