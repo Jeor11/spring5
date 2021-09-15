c189c152c58cec863631e9a33001e9155300dfeb
Joel <jeor_eli@hotmail.com>
2021-09-14 21:05:30 -0500

    Se crea el atributo titulo.

--- angular/clientes-app/src/app/header/header.component.ts
+++ angular/clientes-app/src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+titulo : string = 'App Angular Spring'
   constructor() { }
 
   ngOnInit(): void {
