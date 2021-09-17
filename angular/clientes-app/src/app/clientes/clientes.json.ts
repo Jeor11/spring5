ad06b85e3184f9297b70ff88cec19ceaff565945
Joel <jeor_eli@hotmail.com>
2021-09-17 16:13:10 -0500

    Se agrega al .json los datos.

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import {Cliente} from './clientes';
+
+export const CLIENTES: Cliente[]=[
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'},
+  {id: 1, nombre:'Joel', apellido:'Olguin', createAt:'20-03-2020', email:'jow@gmail.com'}
+];
