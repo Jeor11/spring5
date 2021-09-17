52ca3e46647fd3d22b7b63e750d87d22a2b3ac97
Joel <jeor_eli@hotmail.com>
2021-09-17 14:30:02 -0500

    Se crea el método setHabilitar.

--- angular/clientes-app/src/app/directiva/directiva.component.ts
+++ angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 export class DirectivaComponent  {
   listaCurso: string[]=['TypeScript','JavaScript','PHP','JavaSE', 'C#'];
+  habilitar: boolean = true;
 constructor() { }
+
+setHabilitar(): void {
+   this.habilitar = (this.habilitar == true)? false:true;
+}
 }
