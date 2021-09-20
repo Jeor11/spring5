5ce6fa2e6298348d728c346a60d57aad0f8900f4
Joel <jeor_eli@hotmail.com>
2021-09-20 16:10:01 -0500

    El arreglo Clientes se vuelve observable para ser dinámico.

--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './clientes';
+import {of, Observable} from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
-getClientes(): Cliente[] {
-  return CLIENTES;
+constructor() { }
+
+getClientes(): Observable<Cliente[]> {
+  return of(CLIENTES);
 };
 }
